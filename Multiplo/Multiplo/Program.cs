f1e6eb8370096cf0aa0af989b8432d7a10dfd0e4
Alejandro Perez Gomez <alex98.perez@hotmail.com>
2019-04-22 13:15:42 -0600

    commit 1

---
+++ Multiplo/Multiplo/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections;
+
+namespace Multiplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string text = System.IO.File.ReadAllText(@"C:\Users\Alejandro Perez G\Documents\lectura.txt");
+            System.Console.WriteLine(text);
+
+            string[] numbers = text.Split();
+
+
+            foreach (string number in numbers)
+            {
+
+
+                Console.WriteLine(number);
+
+            }
+
+            //var temp = new List<string>();
+            //foreach (var s in numbers)
+            //{
+            //    if (!string.IsNullOrEmpty(s))
+            //        temp.Add(s);
+            //}
+            //numbers = temp.ToArray();
+
+            //Console.WriteLine(numbers);
+
+
+            //int[] myInts = Array.ConvertAll(numbers, int.Parse);
+
+
+
+
+
+
+        }
+    }
+}
